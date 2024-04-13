5e162a1c53ed0ad6f7e8d76ba62a8e1e654549e9
Poorab Kumar <99170869+poorabkumar@users.noreply.github.com>
2024-04-14 01:08:29 +0530

    Add files via upload

---
+++ frontend/src/lib/seasons.jsx
@@ -0,0 +1,79 @@
+const seasons =[
+    {
+        "id":1,
+        "path":"s1",
+        "name":"Season 1"
+    },
+    {
+        "id":2,
+        "path":"s2",
+        "name":"Season 2"
+    },
+    {
+        "id":3,
+        "path":"s3",
+        "name":"Season 3"
+    },
+    {
+        "id":4,
+        "path":"s4",
+        "name":"Season 4"
+    },
+    {
+        "id":5,
+        "path":"s5",
+        "name":"Season 5"
+    },
+    {
+        "id":6,
+        "path":"s6",
+        "name":"Season 6"
+    },
+    {
+        "id":7,
+        "path":"s7",
+        "name":"Season 7"
+    },
+    {
+        "id":8,
+        "path":"s8",
+        "name":"Season 8"
+    },
+    {
+        "id":9,
+        "path":"s9",
+        "name":"Season 9"
+    },
+    {
+        "id":10,
+        "path":"s10",
+        "name":"Season 10"
+    },
+    {
+        "id":11,
+        "path":"s11",
+        "name":"Season 11"
+    },
+    {
+        "id":12,
+        "path":"s12",
+        "name":"Season 12"
+    },
+    {
+        "id":13,
+        "path":"s13",
+        "name":"Season 13"
+    },
+    {
+        "id":14,
+        "path":"s14",
+        "name":"Season 14"
+    },
+    {
+        "id":15,
+        "path":"s15",
+        "name":"Season 15"
+    }
+]
+
+export default seasons;
